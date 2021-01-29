ac5287bfa16e92f7f8d0f9c0b4ba0e91ec2d65bf
Kamil Przybyl <kamiljanprzybyl@gmail.com>
2021-01-29 12:41:17 +0100

    utils directory added

--- srcs/utils/create_blocks.c
+++ srcs/utils/create_blocks.c
@@ -1,4 +1,6 @@
 #include "malloc.h"
+#include <stdio.h>
+//  This function creates linked list of blocks metadata
 
 void create_blocks(size_t heap_size, void *heap, size_t block_size)
 {
--- srcs/utils/find_free_block.c
+++ srcs/utils/find_free_block.c
@@ -1,4 +1,5 @@
 #include "malloc.h"
+#include <stdio.h>
 
 void *find_free_block(t_block *heap, size_t size)
 {
