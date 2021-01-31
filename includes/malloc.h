ea53b84731ec60ccb2107f365697829f5a53d88c
Kamil Przybyl <kamiljanprzybyl@gmail.com>
2021-01-31 16:58:51 +0100

    ret_mem_block.c functions inhanced

--- includes/malloc.h
+++ includes/malloc.h
@@ -16,13 +16,13 @@ typedef struct          s_block
 
 typedef struct          s_heap
 {
-    int                 *used_blocks;
+    int                 used_blocks;
     struct s_heap       *next;
 
 }                       t_heap;
 
 #define TINY_HEAP_SIZE (2 * getpagesize())
-#define TINY_BLOCK_SIZE ((TINY_HEAP_SIZE - sizeof(t_heap)) / 4)
+#define TINY_BLOCK_SIZE ((TINY_HEAP_SIZE - sizeof(t_heap)) / 112)
 #define SMALL_HEAP_SIZE (4 * getpagesize())
 #define SMALL_BLOCK_SIZE ((SMALL_HEAP_SIZE - sizeof(t_heap)) / 112)
 #define LARGE_HEAP_SIZE getpagesize() - sizeof(t_heap)
@@ -36,12 +36,10 @@ void free(void *ptr);
 void *realloc(void *ptr, size_t size);
 // void show_alloc_mem()
 
-// void create_blocks(size_t heap_size, void *heap, size_t block_size);
 void *find_free_block(t_heap *heap, size_t size);
 void *setup_heap(size_t heap_size, size_t block_size);
-// void merge_heaps(t_block *heap, t_block *new_heap);
 void *ret_tiny_block(size_t size);
-// void *ret_small_block(size_t size);
-// void *ret_large_block();
+void *ret_small_block(size_t size);
+void *ret_large_block();
 
 #endif
--- srcs/utils/ret_mem_block.c
+++ srcs/utils/ret_mem_block.c
@@ -2,77 +2,93 @@
 #include <stdio.h>
 
 t_heap     *tiny;
-// t_heap     *small;
-// t_heap     *large;
+t_heap     *small;
+t_heap     *large;
 
 void *ret_tiny_block(size_t size)
 {
-    printf("1\n");
-    void    *ptr;
+    void        *ptr;
+    t_heap      *heap;
 
-    if (!tiny)      // if no heap exist yet
+    if (!tiny || !(ptr = find_free_block(tiny, size)))      // if heap doesn't exist or is full - create one
     {
-        printf("heap doesn't exist - let's create!\n");
-        tiny = setup_heap(TINY_HEAP_SIZE, TINY_BLOCK_SIZE);
-        tiny->next = NULL;
-    }
-    printf("2\n");
-    if (!(ptr = find_free_block(tiny, size)))       // if heap is full
-    {
-        printf("heap full - let's create another one!\n");
-        t_heap  *new_tiny;
+        heap = setup_heap(TINY_HEAP_SIZE, TINY_BLOCK_SIZE);     // creates heap and saves it in temporary heap var
 
-        printf("a\n");
-        new_tiny = setup_heap(TINY_HEAP_SIZE, TINY_BLOCK_SIZE);
-        printf("b\n");
-        t_heap *temp_tiny = tiny;
-        while (temp_tiny->next)
-            temp_tiny = temp_tiny->next;
-        printf("c\n");
-        temp_tiny->next = new_tiny;
-        printf("d\n");
-        ptr = find_free_block(tiny, size);
-        printf("e\n");
+        if (!tiny)      // if heap doesn't existed earlier - add to the linked list as our fist heap
+        {
+            tiny = heap;
+            tiny->next = NULL;
+        }
+        else        // otherwise ad to the linked list as our last heap
+        {
+            t_heap  *temp_tiny;
+
+            temp_tiny = tiny;
+            while (temp_tiny->next)
+                temp_tiny = temp_tiny->next;
+            temp_tiny->next = heap;      
+        }
+        ptr = ret_tiny_block(size);     // go recursive and find free block on newly created heap
     }
-    printf("3\n");
 
     return (ptr);    
 }
 
-// void *ret_small_block(size_t size)
-// {
-//     void    *ptr;
+void *ret_small_block(size_t size)
+{
+    void        *ptr;
+    t_heap      *heap;
 
-//     if (!small)
-//     {
-//         small = mmap(NULL, SMALL_HEAP_SIZE, PROT_READ | PROT_WRITE, 
-//                     MAP_PRIVATE | MAP_ANON, -1, 0);
-//         create_blocks(SMALL_HEAP_SIZE, small, SMALL_BLOCK_SIZE);
-//     }
+    if (!small || !(ptr = find_free_block(small, size)))      // if heap doesn't exist or is full - create one
+    {
+        heap = setup_heap(SMALL_HEAP_SIZE, SMALL_BLOCK_SIZE);     // creates heap and saves it in temporary heap var
 
-//     if (!(ptr = find_free_block(small, size)))
-//     {
-//         t_block     *new_small;
+        if (!small)      // if heap doesn't existed earlier - add to the linked list as our fist heap
+        {
+            small = heap;
+            small->next = NULL;
+        }
+        else        // otherwise ad to the linked list as our last heap
+        {
+            t_heap  *temp_small;
 
-//         new_small = mmap(NULL, SMALL_HEAP_SIZE, PROT_READ | PROT_WRITE, 
-//                         MAP_PRIVATE | MAP_ANON, -1, 0);
-//         create_blocks(SMALL_HEAP_SIZE, new_small, SMALL_BLOCK_SIZE);
-//         merge_heaps(small, new_small);
-//         ptr = find_free_block(small, size);
-//     }
+            temp_small = small;
+            while (temp_small->next)
+                temp_small = temp_small->next;
+            temp_small->next = heap;      
+        }
+        ptr = ret_small_block(size);     // go recursive and find free block on newly created heap
+    }
 
-//     return (ptr);    
-// }
+    return (ptr);     
+}
 
 
-// void *ret_large_block()
-// {
-//     void    *ptr;
+void *ret_large_block()
+{
+    void        *ptr;
+    t_heap      *heap;
 
-//     large = mmap(NULL, LARGE_HEAP_SIZE, PROT_READ | PROT_WRITE,
-//                 MAP_PRIVATE | MAP_ANON, -1, 0);
-//     // add to linked list which keeps track of large heaps
-//     ptr = large;
+    heap = mmap(NULL, LARGE_HEAP_SIZE, PROT_READ | PROT_WRITE,
+                MAP_PRIVATE | MAP_ANON, -1, 0);
 
-//     return (ptr);
-// }
+    ptr = heap + sizeof(t_heap);
+    heap->used_blocks = 1;
+
+    if (!large)      // if heap doesn't existed earlier - add to the linked list as our fist heap
+    {
+        large = heap;
+        large->next = NULL;
+    }
+    else        // otherwise ad to the linked list as our last heap
+    {
+        t_heap  *temp_large;
+
+        temp_large = large;
+        while (temp_large->next)
+            temp_large = temp_large->next;
+        temp_large->next = heap;      
+    }
+
+    return (ptr);    
+}
